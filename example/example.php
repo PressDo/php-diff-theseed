9ba87c6bbed83d57733777b876e4b44f70924cfc
PRASEOD- <aaei924@gmail.com>
2021-07-02 17:07:47 +0900

    Update example.php

--- example/example.php
+++ example/example.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
@@ -12,7 +11,7 @@
 		<?php
 
 		// Include the diff class
-		require_once dirname(__FILE__).'/../lib/Diff.php';
+		require dirname(__FILE__).'/Diff.php';
 
 		// Include two sample files for comparison
 		$a = explode("\n", file_get_contents(dirname(__FILE__).'/a.txt'));
@@ -26,44 +25,15 @@
 
 		// Initialize the diff class
 		$diff = new Diff($a, $b, $options);
-
-		?>
-		<h2>Side by Side Diff</h2>
-		<?php
-
-		// Generate a side by side diff
-		require_once dirname(__FILE__).'/../lib/Diff/Renderer/Html/SideBySide.php';
-		$renderer = new Diff_Renderer_Html_SideBySide;
-		echo $diff->Render($renderer);
-
 		?>
 		<h2>Inline Diff</h2>
 		<?php
 
 		// Generate an inline diff
-		require_once dirname(__FILE__).'/../lib/Diff/Renderer/Html/Inline.php';
+		require_once dirname(__FILE__).'/Inline.php';
 		$renderer = new Diff_Renderer_Html_Inline;
 		echo $diff->render($renderer);
 
 		?>
-		<h2>Unified Diff</h2>
-		<pre><?php
-
-		// Generate a unified diff
-		require_once dirname(__FILE__).'/../lib/Diff/Renderer/Text/Unified.php';
-		$renderer = new Diff_Renderer_Text_Unified;
-		echo htmlspecialchars($diff->render($renderer));
-
-		?>
-		</pre>
-		<h2>Context Diff</h2>
-		<pre><?php
-
-		// Generate a context diff
-		require_once dirname(__FILE__).'/../lib/Diff/Renderer/Text/Context.php';
-		$renderer = new Diff_Renderer_Text_Context;
-		echo htmlspecialchars($diff->render($renderer));
-		?>
-		</pre>
 	</body>
 </html>
